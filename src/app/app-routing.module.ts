8355d5a781d2d8d95e968f7f8611142ddc514525
CarGDev <ingecarlos.gutierrez@gmail.com>
2021-01-19 20:52:48 -0600

    Adding the structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component'
+
 const routes: Routes = [
   {
     path: '',
